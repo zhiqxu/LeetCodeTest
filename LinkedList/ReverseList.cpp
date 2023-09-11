c01702b20900e17118e68fe6ae44a3a3a585946e
zhiqiaxu <1625375756@qq.com>
2023-09-12 00:30:57 +0800

    add the ReverseList.cpp

---
+++ LinkedList/ReverseList.cpp
@@ -0,0 +1,68 @@
+/**
+ * LeetCode NO.206 反转链表：简单
+ *
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ * 示例 2：
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ *
+ * 示例 3：
+ * 输入：head = []
+ * 输出：[]
+ *
+ */ 
+
+#include <iostream>
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+
+class Solution{
+public:
+    // 双指针法
+    ListNode *reverseList(ListNode *head) {
+        ListNode* temp; // 保存cur的下一个节点
+        ListNode* cur = head;
+        ListNode* pre = NULL;
+        while(cur) {
+            temp = cur->next;  // 保存一下 cur的下一个节点，因为接下来要改变cur->next
+            cur->next = pre; // 翻转操作
+            // 更新pre 和 cur指针
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+
+    // 递归法
+    ListNode *reverse(ListNode *pre, ListNode *cur){
+        if(cur == nullptr) return pre;
+        ListNode *temp = cur->next;
+        cur->next = pre;
+        // 可以和双指针法的代码进行对比，如下递归的写法，其实就是做了这两步
+        // pre = cur;
+        // cur = temp;
+        return reverse(cur, temp);
+    }
+
+    ListNode *reverseList2(ListNode *head){
+        // 和双指针法初始化是一样的逻辑
+        // ListNode* cur = head;
+        // ListNode* pre = NULL;
+        return reverse(nullptr, head);
+    }
+};
